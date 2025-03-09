065b0b54a724bb264d963a84acd8c0cfe5c25ded
TrangLe265 <trang.le@myy.haaga-helia.fi>
2025-03-09 13:10:30 +0200

    Added some views

--- src/main/java/com/trangle/bookdb/web/ViewController.java
+++ src/main/java/com/trangle/bookdb/web/ViewController.java
@@ -5,8 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.trangle.bookdb.domain.Book;
 import com.trangle.bookdb.domain.BookRepository;
 import com.trangle.bookdb.domain.CategoryRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
 
 @Controller
 public class ViewController {
@@ -26,12 +36,34 @@ public class ViewController {
         return "login";
     }	
 	
-	
-    @RequestMapping(value="/books")
+    @RequestMapping(value="/booklists")
     public String bookLists(Model model) {
   	
         model.addAttribute("books", bookRepository.findAll());
         return "booklists";
-    }//
+    }
+
+    @RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        bookRepository.deleteById(id);
+        return "redirect:../booklists"; //redirect to the booklists mapping, not the html index
+    }
+
+    @RequestMapping(value="/add", method=RequestMethod.GET)
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        model.addAttribute("category", categoryRepository.findAll());
+        return "addbook";
+    }
+
+    @RequestMapping(value="/save", method=RequestMethod.POST)
+    public String saveBook(Book book){
+        bookRepository.save(book); 
+        return "redirect:../booklists"; 
+    }
+    
+    
+    
+    
 
 }
